0b3256fef2223d34ca345fae722c4d9a3a87a472
SIDDHARTH SHEKHAR <75613480+siddharttth@users.noreply.github.com>
2024-03-10 03:26:11 +0530

    Add files via upload

---
+++ src/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Navbar'
+import Footer from '../Footer'
+
+export default function home() {
+  return (
+    <div>
+        <div> <Navbar/> </div>
+        <div>body</div>
+        <div>Footer</div>
+    </div>
+  )
+}
